97f0bc65b37aca53d12f8f9fcc0a193e60cef259
Maria94Tom <mariatom94@gmail.com>
2025-01-13 16:52:39 +0530

    commit labassign 1

--- Day3/src/Question4/Book.java
+++ Day3/src/Question4/Book.java
@@ -54,4 +54,25 @@ public class Book {
                 ", price=" + price +
                 '}';
     }
+    //write method searchBook(book id)
+    public void searchBook(int id) {
+        ClassValue<T> llb;
+        if(id.equals(llb.get(0))){ {
+            System.out.println("Book found");
+        }
+        else{
+            System.out.println("Book not found");
+        }
+
+    }
+    //write method sellBook(String isbn, int noOfCopies)
+    public void sellBook(String ISBN, int noOfCopies) throws NotSufficientBookException{
+        if(noOfCopies<0) {
+            throw new NotSufficientBookException("Not sufficient book");
+        }
+        else {
+            System.out.println("Book sold");
+        }
+    }
 }
+
