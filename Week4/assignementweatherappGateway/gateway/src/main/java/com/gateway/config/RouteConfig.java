19a319aecffc21b64484c1c79a163194f39297b0
Maria94Tom <mariatom94@gmail.com>
2025-02-09 17:25:58 +0530

    Day9

--- Week4/assignementweatherappGateway/gateway/src/main/java/com/gateway/config/RouteConfig.java
+++ Week4/assignementweatherappGateway/gateway/src/main/java/com/gateway/config/RouteConfig.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -13,17 +15,22 @@ public class RouteConfig {
     public RouteLocator productAppRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p->p
-                .path("/weatherservice/**")
-                .filters(f->f.rewritePath("/weatherservice/(?<segment>.*)",
+                .path("/weatherservice/weathersms/**")
+                .filters(f->f.rewritePath("/weatherservice/weathersms/(?<segment>.*)",
                         "/${segment}")
-                        .addResponseHeader("X-Response-Header", LocalDateTime.now().toString()))
+                        .addResponseHeader("X-Response-Header", LocalDateTime.now().toString())
+                        .retry(config -> config.setRetries(3)
+                                .setMethods(HttpMethod.GET)
+                                .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true))
+                        .circuitBreaker(config -> config.setName("mycircuitbreaker")
+                                .setFallbackUri("forward:/contactSupport")))
                 .uri("lb://WSERVICES"))
 
 
 
                 .route(p->p
-                        .path("/weatherclient/**")
-                        .filters(f->f.rewritePath("/weatherclient/(?<segment>.*)","/${segment}")
+                        .path("/weatherclient/weathersms**")
+                        .filters(f->f.rewritePath("/weatherclient/weathersms(?<segment>.*)","/${segment}")
                                 .addResponseHeader("X-Response-Header", LocalDateTime.now().toString()))
                         .uri("lb://WCLIENT"))
                 .build();
