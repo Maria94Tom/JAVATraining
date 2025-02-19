0400079042bf6ea423765befd80f16c3ca362d5b
Maria94Tom <mariatom94@gmail.com>
2025-02-19 23:22:27 +0530

    assignment3

--- Week6/productapp-assignment3/src/main/java/com/productapp/ProductappApplication.java
+++ Week6/productapp-assignment3/src/main/java/com/productapp/ProductappApplication.java
@@ -12,8 +12,13 @@ import java.awt.print.Book;
 @SpringBootApplication
 public class ProductappApplication implements CommandLineRunner {
 
-	@Autowired
+
 	private ProductRepo productRepo;
+
+	@Autowired
+	public ProductappApplication(ProductRepo productRepo) {
+		this.productRepo = productRepo;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductappApplication.class, args);
@@ -24,9 +29,9 @@ public class ProductappApplication implements CommandLineRunner {
 		//String name, Integer qty, String vendor, Double cost
 
 		//insert();
-	}
+	//}
 
-	private void insert() {
+	//private void insert() {
 		productRepo.save(new Product("dell laptop", 30, "AB electronic", 5000000.0));
 		productRepo.save(new Product("mouse", 300, "MA electronic", 500.0));
 		productRepo.save(new Product("Keyboard", 20, "ZA electronic", 600.0));
