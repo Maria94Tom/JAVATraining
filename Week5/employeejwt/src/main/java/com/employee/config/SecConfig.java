f2533e35878800067fa09b18086fe945ca582477
Maria94Tom <mariatom94@gmail.com>
2025-02-24 00:49:21 +0530

    Assignment 5_2

--- Week5/employeejwt/src/main/java/com/employee/config/SecConfig.java
+++ Week5/employeejwt/src/main/java/com/employee/config/SecConfig.java
@@ -3,9 +3,11 @@ package com.employee.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +36,11 @@ public class SecConfig {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception {
+        return config.getAuthenticationManager();
     }
 
     public PasswordEncoder passwordEncoder(){
